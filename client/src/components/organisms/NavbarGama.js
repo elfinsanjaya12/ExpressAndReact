d2c05b0ce1cced2c664139e9f53c3c65c6e7e943
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-02-14 11:44:16 +0700

    Navbar

---
+++ client/src/components/organisms/NavbarGama.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import {
+  Navbar,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+  Nav
+} from "react-bootstrap";
+
+class NavbarGama extends Component {
+  render() {
+    return (
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Produk</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default NavbarGama;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import NavbarGama from "../components/organisms/NavbarGama";
+
+class Home extends Component {
+  render() {
+    return <NavbarGama />;
+  }
+}
+
+export default Home;
