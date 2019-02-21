0b03174d24ea0b754ee71daf8a71cfb6b8138b90
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-02-21 09:56:58 +0700

    Proses delete product

--- client/src/components/organisms/TableWithAction.js
+++ client/src/components/organisms/TableWithAction.js
@@ -3,18 +3,33 @@ import Thead from "../molecules/Thead";
 import Tbody from "../molecules/TbodyWithAction";
 
 class TableWithAction extends Component {
-  render() {
-    const { data, thead, tbody } = this.props;
+	render() {
+		const {
+			actionNotDisplay,
+			data,
+			thead,
+			tbody,
+			editUrl,
+			deleteAction,
+			customAction
+		} = this.props;
 
-    return (
-      <div className="table-responsive">
-        <table className="table table-dark">
-          <Thead text={thead} />
-          <Tbody data={data} display={tbody} />
-        </table>
-      </div>
-    );
-  }
+		return (
+			<div className="table-responsive">
+				<table className="table table-dark">
+					<Thead text={thead} />
+					<Tbody
+						data={data}
+						display={tbody}
+						editUrl={editUrl}
+						deleteAction={deleteAction}
+						actionNotDisplay={actionNotDisplay}
+						customAction={customAction}
+					/>
+				</table>
+			</div>
+		);
+	}
 }
 
 export default TableWithAction;
--- client/src/pages/product/index.js
+++ client/src/pages/product/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "../../axios";
 import { Link } from "react-router-dom";
 import Table from "../../components/organisms/TableWithAction";
 import { SearchInput } from "../../components/atoms/Input/index";
@@ -6,17 +7,41 @@ import BreadCrumb from "../../components/atoms/BreadCrumb/BreadCrumb";
 import { connect } from "react-redux";
 import { setAllProduks } from "../../store/actions";
 import { bindActionCreators } from "redux";
+
 class Produk extends Component {
 	constructor() {
 		super();
 		this.state = {
-			query: ""
+			query: "",
+			access: {
+				tambah: false,
+				edit: false,
+				hapus: true
+			}
 		};
 	}
 
 	componentDidMount() {
 		this.props.setAllProduks();
 	}
+
+	_handleDelete = (id) => {
+		// const token = localStorage.token;
+		// const headers = {
+		// 	token,
+		// 	otoritas: "delete_supplier"
+		// };
+		console.log("js");
+		axios
+			.delete(`/product/${id}`)
+			.then((res) => {
+				console.log("dalam");
+
+				this.props.setAllProduks();
+			})
+			.catch((err) => console.log(err));
+	};
+
 	render() {
 		const { produk } = this.props;
 
@@ -36,6 +61,11 @@ class Produk extends Component {
 					data={produk}
 					thead={["Nama", "Ukuran", "Aksi"]}
 					tbody={["name", "width"]}
+					// editUrl={this.state.access.edit ? "/supplier/edit" : null}
+					handlePageClick={this.handlePageClick}
+					deleteAction={
+						this.state.access.hapus ? (id) => this._handleDelete(id) : null
+					}
 				/>
 			</div>
 		);
